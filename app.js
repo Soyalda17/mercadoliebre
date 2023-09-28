416b725b8db3c11f133055836f2b50407cf880ad
Aldair Becerra <soyaldair17@gmail.com>
2023-09-28 15:08:18 -0500

    codigo arreglado con la ruta de imagenes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path'); 
 
-app.listen(3001);
-app.use(express.static(path.resolve(__dirname, '/.public')));
+app.listen(3002);
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))});
