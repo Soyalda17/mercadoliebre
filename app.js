df105ec64a4beda4d5af752e753496a7f3d641c8
Aldair Becerra <soyaldair17@gmail.com>
2024-02-13 00:43:24 -0500

    actualizando mercado liebre

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require('express'); 
-const app = express();
-const path = require('path'); 
+const { log } = require('console');
+const express = require('express');
+const path = require('path');
 
-app.listen(3002);
+const app = express();
+
+app.listen(3003, () => console.log('Sevidor Corriendo En El Puerto 3003'));
+
+
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))});
+    res.sendFile(path.resolve(__dirname, 'src/views/index.html'))});
